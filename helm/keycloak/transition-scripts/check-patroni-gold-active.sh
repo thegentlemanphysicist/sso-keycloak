d8e4edc8f934e402a9f98f92ad806b1983508af0
Jonathan Sharman <jon.w.sharman@gmail.com>
2022-06-15 12:55:22 -0700

    added echo statements

--- helm/keycloak/transition-scripts/check-patroni-gold-active.sh
+++ helm/keycloak/transition-scripts/check-patroni-gold-active.sh
@@ -11,17 +11,19 @@ if ! check_kube_context "api-gold-devops-gov-bc-ca"; then
     echo "invalid context"
     exit 1
 fi
-STATE=$(kubectl -n  ${NAMESPACE} exec sso-patroni-0 -- curl -s http://localhost:8008/patroni)
+STATE=$(kubectl -n  ${NAMESPACE} exec sso-patroni-0 -- curl -s http://localhost:8008/patroni) | jq .state 
 
 # STATE=$(oc rsh -n ${NAMESPACE} sso-patroni-0 curl -s http://localhost:8008/patroni | jq .state) 
+echo "The state is ${STATE}"
 
-if [${STATE} | jq .state != "running"]; then
+if [${STATE} != "running"]; then
     echo "The gold patroni pods must be running"
     exit 1
 fi
 
 #TODO If the connection fails entirely this will default to null, must add a check for the success
 GOLDCONFIG=$(kubectl -n  ${NAMESPACE} exec sso-patroni-0 -- curl -s http://localhost:8008/config)
+echo ${GOLDCONFIG}
 STANDBY_CLUSTER = ${GOLDCONFIG} | jq .standby_cluster
 if [ -z ${STANDBY_CLUSTER} ]; then
     echo "The gold patroni pods must not be in standby mode"
