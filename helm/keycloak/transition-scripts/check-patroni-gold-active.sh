3320b90b8075992675c5a0d65a6f07818fd9d16e
Jonathan Sharman <jon.w.sharman@gmail.com>
2022-06-15 12:58:36 -0700

    removed checks

--- helm/keycloak/transition-scripts/check-patroni-gold-active.sh
+++ helm/keycloak/transition-scripts/check-patroni-gold-active.sh
@@ -16,19 +16,19 @@ STATE=$(kubectl -n  ${NAMESPACE} exec sso-patroni-0 -- curl -s http://localhost:
 # STATE=$(oc rsh -n ${NAMESPACE} sso-patroni-0 curl -s http://localhost:8008/patroni | jq .state) 
 echo "The state is ${STATE}"
 
-if [${STATE} != "running"]; then
-    echo "The gold patroni pods must be running"
-    exit 1
-fi
+# if [${STATE} != "running"]; then
+#     echo "The gold patroni pods must be running"
+#     exit 1
+# fi
 
 #TODO If the connection fails entirely this will default to null, must add a check for the success
 GOLDCONFIG=$(kubectl -n  ${NAMESPACE} exec sso-patroni-0 -- curl -s http://localhost:8008/config)
 echo ${GOLDCONFIG}
 STANDBY_CLUSTER = ${GOLDCONFIG} | jq .standby_cluster
-if [ -z ${STANDBY_CLUSTER} ]; then
-    echo "The gold patroni pods must not be in standby mode"
-    exit 1
-fi
+# if [ -z ${STANDBY_CLUSTER} ]; then
+#     echo "The gold patroni pods must not be in standby mode"
+#     exit 1
+# fi
 
 #TODO: Check that the TSC service is running?
 
