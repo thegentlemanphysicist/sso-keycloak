167de70385ee19ad3f196a38490d9eadfd6ebd83
Jonathan Sharman <jon.w.sharman@gmail.com>
2022-06-16 13:35:09 -0700

    Fix echo

--- helm/keycloak/transition-scripts/check-patroni-gold-active.sh
+++ helm/keycloak/transition-scripts/check-patroni-gold-active.sh
@@ -31,17 +31,21 @@ RESPONSE_CODE=${OUTPUT: -4}
 GOLDCONFIG=${OUTPUT:0:-3}
 # GOLDCONFIG=$(kubectl -n  ${NAMESPACE} exec sso-patroni-0 -- curl -s http://localhost:8008/config)
 echo ${GOLDCONFIG}
-STANDBY_CLUSTER=$($GOLDCONFIG | jq .standby_cluster )
+STANDBY_CLUSTER=$(echo $GOLDCONFIG | jq .standby_cluster )
 
 
 if [ $RESPONSE != 200 ]; then
     echo "The gold patroni pods did not return a 200 response"
     exit 1
+else
+    echo "Patroni config response returned"
 fi
 
 if [ -z ${STANDBY_CLUSTER} ]; then
     echo "The gold patroni pods must not be in standby mode"
     exit 1
+else 
+    echo "Patroni config in active mode"
 fi
 
 #TODO: Check that the TSC service is running?
