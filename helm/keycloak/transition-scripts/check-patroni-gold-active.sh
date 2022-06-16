1c1ab1fc42cd88cf1f8ff5d549faa80b07c50613
Jonathan Sharman <jon.w.sharman@gmail.com>
2022-06-16 08:32:01 -0700

    try contains

--- helm/keycloak/transition-scripts/check-patroni-gold-active.sh
+++ helm/keycloak/transition-scripts/check-patroni-gold-active.sh
@@ -28,7 +28,7 @@ OUTPUT=$(kubectl -n  ${NAMESPACE} exec sso-patroni-0 -- curl -s http://localhost
 STATE=$(echo $OUTPUT | jq '.state')
 echo "The state is $STATE"
 
-if [[ $STATE != "running" ]]; then
+if [[ $STATE != *"running"* ]]; then
     echo "The gold patroni pods must be running"
     exit 1
 fi
