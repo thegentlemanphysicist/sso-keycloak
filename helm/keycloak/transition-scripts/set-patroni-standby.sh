e325731a14cbd811e878caace7a08ba376d71cf1
Jonathan Sharman <jon.w.sharman@gmail.com>
2022-06-17 15:03:37 -0700

    Fix the set patroni standby script

--- helm/keycloak/transition-scripts/set-patroni-standby.sh
+++ helm/keycloak/transition-scripts/set-patroni-standby.sh
@@ -18,18 +18,18 @@ if ! check_kube_context "api-$CLUSTER-devops-gov-bc-ca"; then
 fi
 
 if [ $CLUSTER == golddr ]; then
-    HOST="sso-patroni-gold.${NAMESPACE}.svc.cluster.local"
+    HOST='"sso-patroni-gold.'${NAMESPACE}'.svc.cluster.local"'
 else
-    HOST="sso-patroni-golddr.${NAMESPACE}.svc.cluster.local"
+    HOST='"sso-patroni-golddr.'${NAMESPACE}'.svc.cluster.local"'
 fi
-echo $HOST
-echo '{"standby_cluster": {"create_replica_methods": ["basebackup_fast_xlog"],"host": "'$HOST'","port": '$PORT'}}'
+
 # Set the patroni config to be in standby mode
 RESPONSE=$(kubectl -n  ${NAMESPACE} exec sso-patroni-0 -- curl -s -w "%{http_code}" -XPATCH -d '{"standby_cluster": {"create_replica_methods": ["basebackup_fast_xlog"],"host": '$HOST',"port": '$PORT'}}' http://localhost:8008/config)
+
 RESPONSE_CODE=${RESPONSE: -3}
 CLUSTERCONFIG=${RESPONSE:0:-3}
 echo "The response code is: "$RESPONSE_CODE
-echo $CLUSTERCONFIG
+
 if [ $RESPONSE_CODE = 200 ]; then
     echo "Patroni-$CLUSTER config response returned"
 else
@@ -38,11 +38,12 @@ else
 fi
 
 STANDBY_CLUSTER_CONFIG_LENGTH=$(echo $CLUSTERCONFIG | jq .standby_cluster | jq length )
+echo "The length of the standby config json is: "$STANDBY_CLUSTER_CONFIG_LENGTH
 
 # The length of the standby config json is 0 when patroni is in active mode
-if [ -z $STANDBY_CLUSTER_CONFIG_LENGTH ]; then
+if [[ $STANDBY_CLUSTER_CONFIG_LENGTH == 0 ]]; then
     echo "The patroni-$CLUSTER config in active mode"
     exit 1
 else
-    echo "The patroni-$CLUSTER  pods must not be in standby mode"
+    echo "The patroni-$CLUSTER is now in standby mode"
 fi
